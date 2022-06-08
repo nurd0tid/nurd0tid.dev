14dba62f5bef804daec5d6f3eee7396bbbaa5e7b
nurd0tid <mnur29054@gmail.com>
2022-06-08 22:33:10 +0700

    404 with dino gif

--- src/pages/404.js
+++ src/pages/404.js
@@ -4,7 +4,7 @@ import Helmet from 'react-helmet'
 import { Layout } from '../components/Layout'
 import { SEO } from '../components/SEO'
 import config from '../utils/config'
-import floppy from '../assets/error-404.png'
+import dino from '../assets/dino-404.gif'
 
 // markup
 export default function FourOhFour() {
@@ -17,13 +17,13 @@ export default function FourOhFour() {
         <header>
           <div className="container">
             <h1>404</h1>
-            <p className="description">This was probably a mistake.</p>
+            <p className="description">Haaaaaaa, where am I.</p>
           </div>
         </header>
 
         <section>
           <div className="container">
-            <img src={floppy} alt="404" className="not-found-image" />
+            <img src={dino} alt="404" className="not-found-image" />
           </div>
         </section>
       </article>
